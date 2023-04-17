bb7ffe844132fc8c9617eec9a29d550cb28b7812
Jakub Wilga <wilga327@wp.pl>
2023-04-17 15:37:10 +0200

    init project and add first components

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import Hero from './components/Hero/Hero.js'
+import SearchForm from './components/SearchForm/SearchForm.js'
+import List from './components/List/List.js'
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />
+    </div>
+  );
+};
+
+export default App;
