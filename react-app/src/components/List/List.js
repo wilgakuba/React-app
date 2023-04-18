9accef7e8a632cea228ad17bbebd4e765209bcbc
Jakub Wilga <wilga327@wp.pl>
2023-04-18 22:32:18 +0200

    Add changes

--- react-app/src/components/List/List.js
+++ react-app/src/components/List/List.js
@@ -1,7 +1,18 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState } from 'react';
 
 const List = () => {
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+  
+    setTimeout(() => {
+      setColumns([...columns, { id: 4, title: 'Test column'}]);
+    }, 2000);
+  
     return (
       <div className={styles.List}>
           <header className={styles.header}>
@@ -9,9 +20,7 @@ const List = () => {
           </header>
           <p className={styles.description}>Interesting things I want to check out</p>
           <section className={styles.columns}>
-            <Column title="Books" icon="book" />
-            <Column title="Movies" icon="film"/>
-            <Column title="Games" icon="gamepad"/>
+          {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
           </section>
       </div>
     );
