931e0e905a7ffd22d4f67a6b82a1b76b253a5db6
Jakub Wilga <wilga327@wp.pl>
2023-04-19 17:46:33 +0200

    Add CardForm

--- react-app/src/components/List/List.js
+++ react-app/src/components/List/List.js
@@ -1,18 +1,53 @@
-import styles from './List.module.scss';
-import Column from '../Column/Column';
-import { useState } from 'react';
+import styles from './List.module.scss'
+import Column from '../Column/Column'
+import ColumnForm from '../ColumnForm/ColumnForm'
+import { useState } from 'react'
+import shortid from 'shortid';
 
 const List = () => {
     const [columns, setColumns] = useState([
-        { id: 1, title: 'Books', icon: 'book' },
-        { id: 2, title: 'Movies', icon: 'film' },
-        { id: 3, title: 'Games', icon: 'gamepad' }
+        {
+            id: 1,
+            title: 'Books',
+            icon: 'book',
+            cards: [
+                { id: 1, title: 'This is Going to Hurt' },
+                { id: 2, title: 'Interpreter of Maladies' }
+            ]
+        },
+        {
+            id: 2,
+            title: 'Movies',
+            icon: 'film',
+            cards: [
+                { id: 1, title: 'Harry Potter' },
+                { id: 2, title: 'Star Wars' }
+            ]
+        },
+        {
+            id: 3,
+            title: 'Games',
+            icon: 'gamepad',
+            cards: [
+                { id: 1, title: 'The Witcher' },
+                { id: 2, title: 'Skyrim' }
+            ]
+        }
     ]);
-  
-    setTimeout(() => {
-      setColumns([...columns, { id: 4, title: 'Test column'}]);
-    }, 2000);
-  
+    const addColumn = newColumn => {
+        setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
+    };
+    const addCard = (newCard, columnId) => {
+        const columnsUpdated = columns.map(column => {
+            if(column.id === columnId)
+                return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }]}
+            else
+                return column
+        })
+    
+        setColumns(columnsUpdated);
+    
+    };
     return (
       <div className={styles.List}>
           <header className={styles.header}>
@@ -20,10 +55,12 @@ const List = () => {
           </header>
           <p className={styles.description}>Interesting things I want to check out</p>
           <section className={styles.columns}>
-          {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+          {columns.map(column => 
+          <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} action={addCard} />)}
           </section>
+          <ColumnForm action={addColumn} />
       </div>
     );
   };
   
-  export default List;
+export default List;
