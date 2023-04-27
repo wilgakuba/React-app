08181a206c96ee594aec5f9e6628ab43dd879425
Jakub Wilga <wilga327@wp.pl>
2023-04-27 22:15:44 +0200

    reducer and dispatcha action

--- react-app/src/components/Column/Column.js
+++ react-app/src/components/Column/Column.js
@@ -11,7 +11,7 @@ const Column = props => {
         <ul className={styles.cards}>
           {cards.map(card => <Card key={card.id} title={card.title}/>)}
           </ul>
-          <CardForm columnId = {props.id} action={props.action}/>
+          <CardForm columnId={props.id} />
         </article>
     );
 };
