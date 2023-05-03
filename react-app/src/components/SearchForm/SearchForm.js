019f071b879b280e8d57b708d8db69a1fe596375
Jakub Wilga <wilga327@wp.pl>
2023-05-03 11:29:19 +0200

    update search string

--- react-app/src/components/SearchForm/SearchForm.js
+++ react-app/src/components/SearchForm/SearchForm.js
@@ -1,11 +1,24 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+//import  updateSearchString  from '../../redux/store';
 
 const SearchForm = () => {
+
+
+  const dispatch = useDispatch();
+  const [phrase, setPhrase] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'UPDATE_SEARCHSTRING', payload : phrase });
+  };
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form className={styles.searchForm}onSubmit={handleSubmit}>
+            <TextInput placeholder="Search..." value={phrase} onChange={e => setPhrase(e.target.value)}/>
             <Button>
                 <span className="fa fa-search" />
             </Button>
--- react-app/src/redux/store.js
+++ react-app/src/redux/store.js
@@ -3,14 +3,16 @@ import initialState from './initialState';
 import shortid from "shortid";
 
 const reducer = (state, action) => {
-    switch(action.type) {
-      case 'ADD_COLUMN':
-        return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
-      case 'ADD_CARD':
-        return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
-        default:
-        return state;
-    }
+  switch(action.type) {
+    case 'ADD_COLUMN':
+      return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
+    case 'ADD_CARD':
+      return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
+    case 'UPDATE_SEARCHSTRING':
+      return { ...state, searchString: action.payload };
+    default:
+    return state;
+  }
 };
 
 
